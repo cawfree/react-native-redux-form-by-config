c422ef3cf44ad41a616df2bbe542a9d8c69590ad
Alexander Thomas <cawfree@Alexanders-MacBook-Pro.local>
2019-08-30 20:55:04 +0100

    Updated defaultTheme exposure to callers.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export { default } from './demo/src/getFormByConfig';
 export { default as defaultValidation } from './demo/src/validation';
 export { default as defaultTransform } from './demo/src/transform';
-export { default as defaultTheme } from './demo/src/theme';
+export { defaultTheme } from './demo/src/theme';
 export { default as defaultTypes } from './demo/src/types';
 export { default as DefaultFieldWrapper } from './demo/src/components/DefaultFieldWrapper';
 export { default as DefaultGrouping } from './demo/src/components/DefaultGrouping';
